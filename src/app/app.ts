9934b0cedc9de13f12d36279aa34286223453d5f
kayod <kay.odenthal@gmx.de>
2025-09-01 10:03:46 +0200

    add data service

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+// file: src/app/app.ts
+import { Component, OnInit, inject } from '@angular/core';
 import {
   MatCell, MatCellDef,
   MatColumnDef,
@@ -9,36 +10,24 @@ import {
   MatRowDef,
   MatTable
 } from '@angular/material/table';
-
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
-
+import { Data, PeriodicElement } from './services/data.service';
 
 @Component({
   selector: 'app-root',
   imports: [MatTable, MatHeaderCell, MatCell, MatColumnDef, MatHeaderRow, MatRow, MatRowDef, MatHeaderRowDef, MatHeaderCellDef, MatCellDef],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  standalone: true
 })
-export class App {
+export class App implements OnInit {
+  private readonly data = inject(Data);
+
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  dataSource: PeriodicElement[] = [];
+
+  ngOnInit(): void {
+    this.data.getElements().subscribe(rows => {
+      this.dataSource = rows;
+    });
+  }
 }
