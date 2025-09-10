f812210ab9744e408d168607e3d91fd4d2ee1ba6
kayod <kay.odenthal@gmx.de>
2025-09-10 10:22:31 +0200

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import {Component, inject, OnInit, signal, viewChild} from '@angular/core';
+import { Component, inject, OnInit, signal, viewChild } from '@angular/core';
 
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import {
   MatCell,
   MatCellDef,
@@ -13,23 +13,23 @@ import {
   MatRowDef,
   MatTable,
 } from '@angular/material/table';
-import {MatPaginator, PageEvent} from '@angular/material/paginator';
-import {MatDivider} from '@angular/material/divider';
-import {MatSort, MatSortHeader, Sort} from '@angular/material/sort';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatDivider } from '@angular/material/divider';
+import { MatSort, MatSortHeader, Sort } from '@angular/material/sort';
 
-import {finalize, forkJoin, Subscription} from 'rxjs';
+import { finalize, forkJoin, Subscription } from 'rxjs';
 
-import {DataApiService} from './services/data-api.service';
-import {LoadingIndicator} from './components/loading-indicator/loading-indicator';
+import { DataApiService } from './services/data-api.service';
+import { LoadingIndicator } from './components/loading-indicator/loading-indicator';
 
 import * as _ from 'underscore';
-import {RelationType} from './enums/relation-type.enum';
-import {ListItemModel} from './models/list-item.model';
-import {RelationApiModel} from './models/api/relation.api-model';
-import {PagedResultApiModel} from './models/api/paged-result.api-model';
-import {NavSectionComponent} from './nav-section/nav-section.component';
-import {DEFAULT_PAGE_INDEX, DEFAULT_PAGE_SIZE} from './constants/api-params.constants';
-import {RowModel} from './models/row.model';
+import { RelationType } from './enums/relation-type.enum';
+import { ListItemModel } from './models/list-item.model';
+import { RelationApiModel } from './models/api/relation.api-model';
+import { PagedResultApiModel } from './models/api/paged-result.api-model';
+import { NavSectionComponent } from './nav-section/nav-section.component';
+import { DEFAULT_PAGE_INDEX, DEFAULT_PAGE_SIZE } from './constants/api-params.constants';
+import { RowModel } from './models/row.model';
 
 @Component({
   selector: 'app-root',
